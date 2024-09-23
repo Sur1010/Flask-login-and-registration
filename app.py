5b34c5ff9999c83a76e62a79a885814408d97395
Sur1010 <sur.stepanyan.20@gmail.com>
2024-09-23 17:06:07 +0400

    added flask email

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask
+from flask_mail import Mail
 from models.user import db
 import secrets
 from flask_migrate import Migrate
 from controllers.auth import auth_bp
 
-
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'your_email@example.com'
+app.config['MAIL_PASSWORD'] = 'your_password'
+app.config['MAIL_DEFAULT_SENDER'] = 'your_email@example.com'
+
+mail = Mail(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sur1234@localhost/user_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
-
 
 app.register_blueprint(auth_bp)
 
--- controllers/auth.py
+++ controllers/auth.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from flask_mail import Mail, Message
 from models.user import User, db
 
 auth_bp = Blueprint('auth', __name__)
+
+mail = Mail()
 
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
@@ -16,8 +19,17 @@ def register():
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
+
+        send_confirmation_email(email)
+
         return redirect(url_for('auth.login'))
     return render_template('register.html')
+
+
+def send_confirmation_email(email):
+    msg = Message("Welcome to Our App", recipients=[email])
+    msg.body = "Thank you for registering!"
+    mail.send(msg)
 
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
@@ -42,5 +54,5 @@ def home():
 
 @auth_bp.route('/logout')
 def logout():
-    session.pop('username', None)
+    session.pop('email', None)
     return redirect(url_for('auth.login'))
