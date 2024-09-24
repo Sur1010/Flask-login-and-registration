abd8dcae81807e50388f40d29e8c4a302c6805a0
Sur1010 <sur.stepanyan.20@gmail.com>
2024-09-24 22:43:37 +0400

    confirm_email

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from flask_mail import Mail, Message
 from models.user import User, db
+import random
+import string
 
 auth_bp = Blueprint('auth', __name__)
-
 mail = Mail()
 
 
@@ -15,21 +16,40 @@ def register():
         if User.query.filter_by(email=email).first():
             flash('Email already exists!')
             return redirect(url_for('auth.register'))
+
+        confirmation_code = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+
         user = User(email=email)
         user.set_password(password)
+        user.confirmation_code = confirmation_code
         db.session.add(user)
         db.session.commit()
 
-        send_confirmation_email(email)
+        send_confirmation_email(email, confirmation_code)
 
+        flash('Registration successful! Please check your email to confirm.')
         return redirect(url_for('auth.login'))
     return render_template('register.html')
 
 
-def send_confirmation_email(email):
-    msg = Message("Welcome to Our App", recipients=[email])
-    msg.body = "Thank you for registering!"
+def send_confirmation_email(email, confirmation_code):
+    confirmation_url = url_for('auth.confirm_email', code=confirmation_code, _external=True)
+    msg = Message("Confirm Your Registration", recipients=[email])
+    msg.body = f"Thank you for registering! Please confirm your registration by clicking the following link: {confirmation_url}"
     mail.send(msg)
+
+
+@auth_bp.route('/confirm/<code>')
+def confirm_email(code):
+    user = User.query.filter_by(confirmation_code=code).first()
+    if user:
+        user.confirmed = True
+        user.confirmation_code = None
+        db.session.commit()
+        flash('Your email has been confirmed! You can now log in.')
+        return redirect(url_for('auth.login'))
+    flash('Invalid confirmation code!')
+    return redirect(url_for('auth.register'))
 
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
@@ -39,6 +59,9 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(email=email).first()
         if user and user.check_password(password):
+            if not user.confirmed:
+                flash('Please confirm your email before logging in.')
+                return redirect(url_for('auth.login'))
             session['email'] = email
             return redirect(url_for('auth.home'))
         flash('Invalid email or password!')
--- models/user.py
+++ models/user.py
@@ -8,6 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
+    confirmation_code = db.Column(db.String(10), nullable=True)
+    confirmed = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
