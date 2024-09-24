cd4d9cafbfec9bf6ba3748bbe2ba63e525cac269
Sur1010 <sur.stepanyan.20@gmail.com>
2024-09-24 21:36:19 +0400

    fixed git conflict

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', secrets.token_hex(16))
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
